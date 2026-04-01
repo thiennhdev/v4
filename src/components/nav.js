398a2ba877d2c892b6e50df5e2a98b73bbfade44
Nguyen Hoang Thien <thiengk563@gmail.com>
2026-04-01 22:42:29 +0700

    Use Nguyen_Hoang_Thien_SE.pdf and remove resume.pdf

--- src/components/nav.js
+++ src/components/nav.js
@@ -206,7 +206,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="/Nguyen_Hoang_Thien_SE.pdf" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
